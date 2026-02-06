bb5a10197e8a9f02b8656cd97078557b8c8c1c0b
chibiro1 <ckandoy@gmail.com>
2026-02-06 19:47:38 +0800

    UI changes 2/6/2026

--- Freaky Bus/Assets/Scripts/MainMenu.cs
+++ Freaky Bus/Assets/Scripts/MainMenu.cs
@@ -1,98 +1,40 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string playSceneName = "Gameplay";
-
-    [SerializeField] private float playClickDelay = 1.2f;
-    [SerializeField] private float optionsClickDelay = 0.4f;
-    [SerializeField] private float exitClickDelay = 0.6f;
-
     [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private SceneFader sceneFader;
 
-    [SerializeField] private GameObject playStripe;
-    [SerializeField] private GameObject optionsStripe;
-    [SerializeField] private GameObject exitStripe;
-
-    private bool isBusy = false;
-
-    private void OnEnable()
-    {
-        ResetStripes();
-        isBusy = false;
-    }
-
-    private void ResetStripes()
-    {
-        if (playStripe != null) playStripe.SetActive(false);
-        if (optionsStripe != null) optionsStripe.SetActive(false);
-        if (exitStripe != null) exitStripe.SetActive(false);
-    }
-
+    // ---------- PLAY ----------
     public void Play()
     {
-        if (isBusy) return;
-
-        isBusy = true;
-        ResetStripes();
-
-        if (playStripe != null)
-            playStripe.SetActive(true);
-
-        StartCoroutine(PlayWithDelay());
+        if (sceneFader != null)
+        {
+            sceneFader.FadeToScene(playSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("SceneFader not assigned!");
+        }
     }
 
-    private IEnumerator PlayWithDelay()
-    {
-        yield return new WaitForSecondsRealtime(playClickDelay);
-        SceneManager.LoadScene(playSceneName);
-    }
-
+    // ---------- OPTIONS ----------
     public void Options()
     {
-        if (isBusy) return;
-
-        isBusy = true;
-        ResetStripes();
-
-        if (optionsStripe != null)
-            optionsStripe.SetActive(true);
-
-        StartCoroutine(OpenOptionsWithDelay());
-    }
-
-    private IEnumerator OpenOptionsWithDelay()
-    {
-        yield return new WaitForSecondsRealtime(optionsClickDelay);
-        optionsPanel.SetActive(true);
-        isBusy = false;
+        if (optionsPanel != null)
+            optionsPanel.SetActive(true);
     }
 
     public void CloseOptions()
     {
-        ResetStripes();
-        optionsPanel.SetActive(false);
-        isBusy = false;
+        if (optionsPanel != null)
+            optionsPanel.SetActive(false);
     }
 
+    // ---------- EXIT ----------
     public void ExitGame()
     {
-        if (isBusy) return;
-
-        isBusy = true;
-        ResetStripes();
-
-        if (exitStripe != null)
-            exitStripe.SetActive(true);
-
-        StartCoroutine(ExitWithDelay());
-    }
-
-    private IEnumerator ExitWithDelay()
-    {
-        yield return new WaitForSecondsRealtime(exitClickDelay);
         Debug.Log("Game Quit");
         Application.Quit();
     }
